4446f93ae15598127667fb49980de035acdc8ff2
K-Kilel <86119948+K-Kilel@users.noreply.github.com>
2024-11-05 01:43:21 -0500

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Asking for the user's inputs
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your location: ")
+
+# Printing the personalized image
+print(f"Hello, {name}! You are {age} years old and live in {location}.")
